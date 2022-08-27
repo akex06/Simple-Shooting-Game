650865dfb3ac3ee039672dc99dd9b9ad9dd6dfbb
Akex <67469268+akex06@users.noreply.github.com>
2022-08-27 14:15:00 +0200

    Added a leaderboard

--- src/scenes.py
+++ src/scenes.py
@@ -8,6 +8,8 @@ from random import randint
 from src.tools import Tools
 
 from src.constants import (
+    BLACK,
+    FRAME_RATE,
     WHITE
 )
 
@@ -23,11 +25,11 @@ class SceneManager:
         self.crosshair = pygame.image.load("assets/crosshair.png").convert()
         self.crosshair = pygame.transform.scale(self.crosshair, (120, 120))
         self.crosshair.set_colorkey(WHITE)
-        self.amount = 0
         self.font = pygame.font.SysFont("Sans", 60)
         self.tools = tools
         self.buttons: Dict[str, pygame.Rect] = {}
-    
+        self.arrow = pygame.transform.scale(pygame.image.load("assets/arrow.png"), (100, 100))
+
     def process_game_interaction(self):
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
@@ -45,12 +47,29 @@ class SceneManager:
                     for name, button in self.buttons.items():
                         if button.collidepoint(event.pos):
                             if name.lower() == "play":
-                                self.scene = "game"
+                                self.scene = "name"
 
                             if name.lower() == "leaderboard":
                                 self.scene = "leaderboard"
-                            
+                
+                if self.scene in ("leaderboard", "finish"):
+                    if self.arrow.get_rect().collidepoint(event.pos):
+                        self.scene = "menu"
 
+            if event.type == pygame.KEYDOWN:
+                if self.scene == "name":
+                    try:
+                        if chr(event.key).upper() in [chr(x) for x in range(65, 91)]:
+                            self.player_name += chr(event.key)
+                        
+                        elif event.key == 13 and len(self.player_name) >= 3:
+                                self.scene = "game"
+                            
+                        elif event.key == 8:
+                            self.player_name = self.player_name[:-1]
+                    
+                    except:
+                        pass        
     def menu(self):
         self.screen.fill(WHITE)
         
@@ -64,9 +83,28 @@ class SceneManager:
             if play_button not in self.buttons.values():
                 self.buttons[text] = play_button
 
+        self.amount = 0
+        self.player_name = ""
+        self.time = 10
+        self.ticks = 0
+
+    def name(self):
+        self.screen.fill(WHITE)
+
+        text = self.tools.get_text("Enter your name", self.font)
+        self.tools.draw(text, (self.screen.get_width()/2 - text.get_width()/2, 200))
+
+        text = self.tools.get_text(self.player_name, self.font)
+        self.tools.draw(text, (self.screen.get_width()/2 - text.get_width()/2 ,280))
+
+
+
     def game(self):
+
         self.screen.fill(WHITE)
         self.tools.draw(self.tools.get_text(f"Amount: {self.amount}", self.font))
+
+        self.tools.draw(self.tools.get_text(str(self.time), self.font), (self.screen.get_width() - 80, 20))
 
         if self.draw:
             for i in range(20):
@@ -90,5 +128,51 @@ class SceneManager:
 
         self.screen.blit(self.crosshair, crosshair_pos)
 
+        self.ticks += 1
+        if self.ticks == FRAME_RATE:
+            self.ticks = 0
+            self.time -= 1
+
+        if self.time == 0:
+            pygame.mouse.set_visible(True)
+            self.scene = "finish"
+
     def leaderboard(self):
         self.screen.fill(WHITE)
+
+        self.screen.blit(self.arrow, (20, 20))
+
+        l = sorted([(v, k) for k, v in lb.items()], reverse = True)
+
+        n = 200
+        for i in l:
+            text = self.tools.get_text(f"{i[1]}: {i[0]}", self.font)
+            self.tools.draw(text, (self.screen.get_width()/2 - text.get_width()/2, n))
+
+            n += 80
+
+    def finish(self):
+        self.screen.fill(WHITE)
+        
+        self.screen.blit(self.arrow, (20, 20))
+
+        if self.player_name in lb:
+            if self.amount > lb[self.player_name]:
+                lb[self.player_name] = self.amount
+        
+        else:
+            lb[self.player_name] = self.amount
+        
+        best = lb[self.player_name]
+
+        with open("src/leaderboard.json", "w") as f:
+            json.dump(lb, f, indent = 4)
+
+        texts = (f"You made {self.amount} points", f"Best: {best}")
+        n = 300
+
+        for text in texts:
+            text = self.tools.get_text(text, self.font)
+            self.tools.draw(text, (self.screen.get_width()/2 - text.get_width()/2, n))
+
+            n += 80
